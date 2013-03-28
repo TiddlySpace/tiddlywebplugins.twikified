b83958697004d7203fed20a3024efe3c653f9535
Ben Paddock <pads@thisispads.me.uk>
2013-03-28 16:16:29 +0000

    Use fully qualified module names
    This is so that the tiddlyweb instance used in the tests can find the renderer

--- tiddlywebconfig.py
+++ tiddlywebconfig.py
@@ -1,6 +1,6 @@
 config = {
-    'wikitext.default_renderer': 'twikified',
+    'wikitext.default_renderer': 'tiddlywebplugins.twikified',
     'wikitext.type_render_map': {
-        'text/x-tiddlywiki': 'twikified'
+        'text/x-tiddlywiki': 'tiddlywebplugins.twikified'
     }
 }
