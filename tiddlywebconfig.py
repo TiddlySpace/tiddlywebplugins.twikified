782b9976d6be61fe5d3e1625013329744c5d5daa
Ben Paddock <pads@thisispads.me.uk>
2013-03-28 15:04:02 +0000

    Map TiddlyWiki file type to twikified

--- tiddlywebconfig.py
+++ tiddlywebconfig.py
@@ -1,3 +1,6 @@
 config = {
-        'wikitext.default_renderer': 'twikified',
-        }
+    'wikitext.default_renderer': 'twikified',
+    'wikitext.type_render_map': {
+        'text/x-tiddlywiki': 'twikified'
+    }
+}
